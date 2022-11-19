6918c36f213cc644377249f2f406be06caa711f0
musammil21 <67472271+musammil21@users.noreply.github.com>
2022-11-19 21:41:13 +0530

    Delete Final Deliverables directory

--- Final Deliverables/sqlite.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 
-
-conn= sqlite3.connect('database.db')
-print('opened')
-
-conn.execute('CREATE TABLE user (name TEXT, email TEXT, phone TEXT, password TEXT)')
-print("table created")
-conn.close()
